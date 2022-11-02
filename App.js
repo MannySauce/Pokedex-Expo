d35a699cf169cfe2bc552c9467f4aefeecfcafaa
MannySauce <33158848+MannySauce@users.noreply.github.com>
2022-11-02 15:54:22 -0700

    added splash screen and app icon, eas configs

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
+import React, { useEffect, useState, useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+//import axios from "axios";
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>¡Bienvenido!</Text>
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e4000f',
     alignItems: 'center',
     justifyContent: 'center',
   },
